e58c707f707b4ae29976f609f9f5658e690554f6
Anna Propas <apropas@gmail.com>
2016-05-24 13:07:12 -0700

    added oauth signing file and twitter remote app object

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -7,34 +7,28 @@
     Create a controller using this template
 """
 from system.core.controller import *
+from flask_oauth import OAuth
+import signing
+
+ck = signing.consumer_key()
+cs = signing.consumer_secret()
+
+oauth = OAuth()
+
+twitter = oauth.remote_app('twitter',
+    base_url='https://api.twitter.com/1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    consumer_key=ck,
+    consumer_secret=cs
+)
 
 class Welcome(Controller):
     def __init__(self, action):
         super(Welcome, self).__init__(action)
-        """
-            This is an example of loading a model.
-            Every controller has access to the load_model method.
-        """
         self.load_model('WelcomeModel')
         self.db = self._app.db
 
-        """
-        
-        This is an example of a controller method that will load a view for the client 
-
-        """
-   
     def index(self):
-        """
-        A loaded model is accessible through the models attribute 
-        self.models['WelcomeModel'].get_users()
-        
-        self.models['WelcomeModel'].add_message()
-        # messages = self.models['WelcomeModel'].grab_messages()
-        # user = self.models['WelcomeModel'].get_user()
-        # to pass information on to a view it's the same as it was with Flask
-        
-        # return self.load_view('index.html', messages=messages, user=user)
-        """
         return self.load_view('index.html')
-
