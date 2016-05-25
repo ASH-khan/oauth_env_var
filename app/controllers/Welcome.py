477b0de8756e70db69545f2d920ae51b62e36095
Anna Propas <apropas@gmail.com>
2016-05-25 08:00:39 -0700

    code better organized, added global client

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -11,24 +11,26 @@ import oauth2 as oauth
 import json
 import signing
 
-CONSUMER_KEY = signing.consumer_key()
-CONSUMER_SECRET = signing.consumer_secret()
-ACCESS_KEY = signing.access_key()
-ACCESS_SECRET = signing.access_secret()
-
-consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
-client = oauth.Client(consumer, access_token)
-
-timeline_endpoint = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-response, data = client.request(timeline_endpoint)
-
 class Welcome(Controller):
     def __init__(self, action):
         super(Welcome, self).__init__(action)
+
+        CONSUMER_KEY = signing.consumer_key()
+        CONSUMER_SECRET = signing.consumer_secret()
+        ACCESS_KEY = signing.access_key()
+        ACCESS_SECRET = signing.access_secret()
+
+        consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+        access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+        global client
+        client = oauth.Client(consumer, access_token)
+
         self.load_model('WelcomeModel')
         self.db = self._app.db
 
     def index(self):
+
+        timeline_endpoint = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        response, data = client.request(timeline_endpoint)
         tweets = json.loads(data)
         return self.load_view('index.html', tweets=tweets)
